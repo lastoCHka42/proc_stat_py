5218ff7add2c7e8cb49a9153dfc35fc55c7b78de
lastoCHka42 <anna@void.so>
2021-10-15 22:52:05 +0300

    main.py

---
+++ main.py
@@ -0,0 +1,52 @@
+import os
+import subprocess
+import psutil
+import time
+
+
+#get as variables: path_to_file, interval time
+path_to_file = input("Path to file:")
+interval_time = input("interval time")
+
+pid = subprocess.Popen(path_to_file)
+
+def stat_for_unix():
+    while True:
+        #get CPU
+        cpu_usage =
+        #get Resident Set Size
+        rs_size = os.system("cat /proc/143311/status | grep VmRSS | awk '{print $2}'") #збс pid захардкодила
+        #get Virtual Memory Size /proc/[pid]/status | grep 'VMSize'
+        vm_size = os.system("cat /proc/143311/status | grep VmSize | awk '{print $2}'")
+        #get open file descriptors count /proc/[pid]/status | grep 'FDSize'
+        fd_count = os.system("cat /proc/143311/status | grep FDSize | awk '{print $2}'")
+
+        result = {'cpu_usage': cpu_usage, 'rs_size': rs_size, "vm_size": vm_size, "fd_count": fd_count}
+        json = json.dump(result)
+        
+        #suspend to json file or to SQLite
+
+        time.sleep(interval_time)
+
+def stat_for_mac:
+    print('This OS type is not supported yet')
+
+
+def stat_for_windows:
+    print('This OS type is not supported yet')
+    #while True:
+        #get CPU
+        #get Resident Set Size
+        #get Virtual Memory Size
+        #get open file descriptors count
+        #suspend to json file or to SQLite
+
+os_type = os.uname()
+if "Linux" in os_type:
+    stat_for_unix()
+elif "Mac" in os_type:
+    stat_for_mac()
+elif "Windows" in os_type:
+    stat_for_windows()
+else:
+    print("This OS type is not supported yet")
